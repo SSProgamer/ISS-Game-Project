303dc517c6d7450873f56ee38662876ddd559e97
WATCHARAWEE SRIPIBOON <>
2022-04-25 15:28:06 +0000

    Update scenes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-room1 = document.getElementById("room1")
+bedroom = document.getElementsByClassName("room")[0]
+gym = document.getElementsByClassName("room")[1]
+cafetaria = document.getElementsByClassName("room")[2]
+lab = document.getElementsByClassName("room")[3]
 
-// room1.style.zIndex = `1`;
+// room1.style.zIndex = `-10`;
